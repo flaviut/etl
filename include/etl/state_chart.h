75aa98a10ee6b9b00f34c4ca74dd8ce763b62633
John Wellbelove <jwellbelove@users.noreply.github.com>
2022-12-10 18:08:53 +0000

    Work in progress

--- include/etl/state_chart.h
+++ include/etl/state_chart.h
@@ -49,8 +49,6 @@ namespace etl
     typedef uint_least8_t state_id_t;
     typedef uint_least8_t event_id_t;
 
-
-
     //*************************************************************************
     /// Transition definition
     //*************************************************************************
